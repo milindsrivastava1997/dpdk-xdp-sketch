94a1f9ec2a25b1de84fb58e7789b89c07a610497
Milind Srivastava <milindsrivastava1997@gmail.com>
2023-05-02 15:19:01 -0400

    Modified XDP compile script to include xxhash external library

--- XDP/CM/compile.sh
+++ XDP/CM/compile.sh
@@ -1,6 +1,6 @@
 MY_SKETCH=Ours
 
 clang -O2 -g -Wall -target bpf -c $MY_SKETCH-XDP.c -o prog.o
-g++ $MY_SKETCH-main.c -o main -lbpf -lxdp
+g++ $MY_SKETCH-main.c -o main -lbpf -lxdp -lxxhash
 
 sudo /local/xdp-tools/xdp-loader/xdp-loader unload -a enp65s0f0np0
