4b8b97f86b2a73fd07cfe53f33656c3c83f4dbd6
Milind Srivastava <milindsrivastava1997@gmail.com>
2023-06-20 13:34:19 -0400

    Changed XDP parse code to include UDP packets too, not just TCP

--- XDP/xdp_common/parse.h
+++ XDP/xdp_common/parse.h
@@ -2,13 +2,13 @@
 #define XDP_PARSE_H
 
 #include <linux/tcp.h>
+#include <linux/udp.h>
 #include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_endian.h>
 
 #include <linux/if_ether.h>
 #include <linux/ip.h>
-#include <linux/tcp.h>
 #include <linux/in.h>
 
 //struct Packet{
@@ -32,19 +32,29 @@ inline void init_key(uint64_t* key, uint64_t src, uint64_t dst) {
 inline int32_t parse_key(struct xdp_md *skb, uint64_t* key){
     void *data_end = (void *)(long)skb->data_end;
     void *data = (void *)(unsigned long long)skb->data;
-
-    if(data + sizeof(struct ethhdr) + sizeof(struct iphdr) + sizeof(struct tcphdr) > data_end)
-        return -1;
-    
     struct iphdr *iph = data + sizeof(struct ethhdr);
-    if(iph->protocol == IPPROTO_TCP){
+
+    if((void*)iph + sizeof(struct iphdr) > data_end) {
+        return -1;
+    }
+    if(iph->protocol == IPPROTO_TCP) {
+        if((void*)iph + sizeof(struct iphdr) + sizeof(struct tcphdr) > data_end)
+            return -1;
         struct tcphdr *h = ((void*)iph) + sizeof(struct iphdr);
         //init_key(key, h->source, h->dest);
         init_key(key, bpf_ntohl(iph->daddr), bpf_ntohs(h->dest));
         return 0;
     }
-
-    return -1;
+    else if(iph->protocol == IPPROTO_UDP) {
+        if((void*)iph + sizeof(struct iphdr) + sizeof(struct udphdr) > data_end)
+            return -1;
+        struct udphdr *h = ((void*)iph) + sizeof(struct iphdr);
+        //init_key(key, h->source, h->dest);
+        init_key(key, bpf_ntohl(iph->daddr), bpf_ntohs(h->dest));
+        return 0;
+    }
+    else 
+        return -1;
 }
 
 #endif
