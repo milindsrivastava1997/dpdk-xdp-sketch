9dabc228619b44b2c68df5d62cc391ddf22bf4c2
Milind Srivastava <milindsrivastava1997@gmail.com>
2023-08-02 21:12:26 -0400

    Updated XDP compile scripts to return non-zero error code if compile fails

--- XDP/Count/compile.sh
+++ XDP/Count/compile.sh
@@ -1,6 +1,8 @@
+set -e
+
 MY_SKETCH=Ours
 
 clang -O2 -g -Wall -target bpf -c $MY_SKETCH-XDP.c -o prog.o
 g++ $MY_SKETCH-main.c -o main -lbpf -lxdp
 
-sudo /local/xdp-tools/xdp-loader/xdp-loader unload -a enp65s0f0np0
+sudo /local/xdp-tools/xdp-loader/xdp-loader unload -a enp65s0f0np0 || :
